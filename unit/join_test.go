fb35ad913a8ea1b498789ca7fe04469deac829ec
index0h <index.0h@gmail.com>
2019-11-26 01:02:11 +0200

    [#20] move to workflow action
    check filepath.Join

--- unit/join_test.go
+++ unit/join_test.go
@@ -1,10 +1,58 @@
 package unit
 
 import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"sort"
 	"testing"
 )
 
 func TestJoin(t *testing.T) {
-	t.Error(filepath.Join("namespace", "data"))
+	rootPath, err := ioutil.TempDir("", "")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	nsPath := filepath.Join(rootPath, "namespace1")
+
+	if err := os.Mkdir(nsPath, 0777); err != nil {
+		t.Error(err)
+	}
+
+	if file, err := os.Create("file.go"); err != nil {
+		t.Error(err)
+	} else {
+		t.Error(file)
+	}
+
+	t.Errorf("%+v", findAllFolders(rootPath))
+}
+
+func findAllFolders(path string) []string {
+	result := []string{}
+
+	err := filepath.Walk(
+		path,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				result = append(result, path)
+			}
+
+			return nil
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	sort.Strings(result)
+
+	return result
 }
